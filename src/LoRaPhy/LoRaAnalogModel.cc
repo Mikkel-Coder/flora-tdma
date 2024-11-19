5810b9f1b59774a09eec674abb3427959a662c9e
Mikkel-Coder <ms22@student.aau.dk>
2024-11-19 14:23:18 +0100

    fixed debug compile warnings (by comments)

--- src/LoRaPhy/LoRaAnalogModel.cc
+++ src/LoRaPhy/LoRaAnalogModel.cc
@@ -91,7 +91,7 @@ W LoRaAnalogModel::computeReceptionPower(const IRadio *receiverRadio, const ITra
     const INarrowbandSignal *narrowbandSignalAnalogModel = check_and_cast<const INarrowbandSignal *>(transmission->getAnalogModel());
     const IScalarSignal *scalarSignalAnalogModel = check_and_cast<const IScalarSignal *>(transmission->getAnalogModel());
     const Coord receptionStartPosition = arrival->getStartPosition();
-    const Coord receptionEndPosition = arrival->getEndPosition();
+    // const Coord receptionEndPosition = arrival->getEndPosition();
 //    const Quaternion transmissionDirection = computeTransmissionDirection(transmission, arrival);
 //    const Quaternion transmissionAntennaDirection = transmission->getStartOrientation() - transmissionDirection;
 //    const Quaternion receptionAntennaDirection = transmissionDirection - arrival->getStartOrientation();
