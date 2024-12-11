ca042a1bfc5d3973b5450325e5e7d02c41e704a6
Mikkel-Coder <ms22@student.aau.dk>
2024-12-11 16:48:25 +0100

    wip. try to get plotting working

--- plot.py
+++ plot.py
@@ -5,6 +5,8 @@ import logging
 import re
 import sys
 
+import matplotlib.pyplot as plt
+
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(logging.INFO)
@@ -14,6 +16,7 @@ path = Path('samples/flora-tdma/simulations/results/')
 json_path: Path = path / 'data_vec.json'
 pickle_path: Path = path / 'data_vec.pkl'
 pickle_path_clean: Path = path / 'data_clean_vec.pkl'
+json_path_clean: Path = path / 'data_clean.json'
 
 ln_re_pattern = r'LoRaNetworkTest\.loRaNodes\[(\d+)\]'
 
@@ -29,10 +32,12 @@ def generate_pickle():
 
 
 def clean_pickle_data():
+    logger.info("Loading dirty pickle data..")
     with pickle_path.open('rb') as fp:
         dirty_data: dict = pickle.load(fp)
 
     # Clean
+    logger.info("Cleaning dirty data...")
     clean_data = dict()
     for key in dirty_data.keys():
         numNodes = dirty_data[key]["itervars"]["numNodes"]
@@ -95,29 +100,59 @@ def clean_pickle_data():
         logger.info(f'Total Power Sum: {total_power_sum}')
         logger.info(f'Total Packet length Sum: {total_packet_length_sum}')
 
+    logger.info("Writing clean pickle data...")
     with pickle_path_clean.open('wb') as fp:
         pickle.dump(clean_data, fp)
 
+    logger.info("Writing clean data to json file...")
+    with json_path_clean.open('w') as fp:
+        json.dump(clean_data, fp)
 
-def plot_power_consumption(data: dict):
-    ...
 
+def plot_power_consumption_per_node(data: dict):
+    x = sorted(int(key) for key in data.keys())
+    y = [data[str(key)]["total_power_sum"] / key for key in x]
+    
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y, marker="o", linestyle="-", color="blue")
+    plt.title("Power Consumption per Node")
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Power Consumption")
+    plt.grid(True)
+    plt.show()
+
+def plot_throughput_per_node(data: dict):
+    x = sorted(int(key) for key in data.keys())
+    time = 6 * 60 * 60
+    y = [data[str(key)]["total_packet_length_sum"] / time for key in x]
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y, marker="o", linestyle="-", color="blue")
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Throughput per Node")
+    plt.grid(True)
+    plt.show()
 
 def main() -> None:
+    # opp_scavetool index flora-tdma-1000.vec
+    # opp_scavetool x -o data_vec.json *.vec
     if not pickle_path.exists():
-        logger.info("Not pickle data found. Generating one...")
+        logger.info("No pickle data found. Generating one...")
         generate_pickle()
 
     if not pickle_path_clean.exists():
-        logger.info("Not clean pickle data found. Cleaning ...")
+        logger.info("No clean pickle data found. Cleaning ...")
         clean_pickle_data()
 
     logger.info("Loading clean pickle data...")
     with pickle_path_clean.open('rb') as fp:
         data: dict = pickle.load(fp)
 
+    print(data)
+
     # plot power consumption per node
-    plot_power_consumption(data)
+    #plot_power_consumption_per_node(data)
+    plot_throughput_per_node(data)
     # plot Network Power Consumption
     # Throughput
 
