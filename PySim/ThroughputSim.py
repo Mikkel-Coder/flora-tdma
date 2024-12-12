ee969fd67bc7445eca998f46b3f7a5ba274d5792
MzKay-Chan <jwcmckay@gmail.com>
2024-12-12 11:56:41 +0100

    Made changes to the math, so that it looks better

--- PySim/ThroughputSim.py
+++ PySim/ThroughputSim.py
@@ -9,50 +9,52 @@ def sim():
     gTime = 0.4
     broadcast = 7
     headerSize = 2
-    packetSize = 254
+    packetSize = 20
+    maxPacketSize = 256
     timeSlots = 100
     packetAirtime = 11.6
     lampda = 1/10**3
 
     periodeTime = broadcast + (gTime + packetAirtime) * timeSlots
-    maxThroughPerTimeSlot = (headerSize + packetSize) / packetAirtime
-    maxThroughForPeriode = (
-        (maxThroughPerTimeSlot * timeSlots) / periodeTime) * 8
 
-    print(f"{maxThroughPerTimeSlot=}")
-    print(f"{maxThroughForPeriode=}")
+    maxThroughputPossible = ((headerSize + maxPacketSize) / packetAirtime) * 8
+    maxThroughputForActualSize = (
+        (headerSize + packetSize) / packetAirtime) * 8
+
+    print(f"{maxThroughputPossible=}")
+    print(f"{maxThroughputForActualSize=}")
 
     for nodecount in range(1, maxNumberOfNodes):
-        print(f"\n{nodecount=}:")
-        perNodeairtime = (timeSlots/nodecount) * \
-            (packetAirtime + gTime)  # Seconds on air
+        # print(f"\n{nodecount=}:")
+        # perNodeairtime = (timeSlots/nodecount) * \
+        #     (packetAirtime + gTime)  # Seconds on air
 
-        print(f"{perNodeairtime=}")
+        # print(f"{perNodeairtime=}")
 
-        dutyTimepercentage = perNodeairtime / periodeTime
+        # dutyTimepercentage = perNodeairtime / periodeTime
 
-        print(f"{dutyTimepercentage=}")
+        # print(f"{dutyTimepercentage=}")
 
         expectedPacketPerNoder = lampda * periodeTime
 
-        print(f"{expectedPacketPerNoder=}")
+        # print(f"{expectedPacketPerNoder=}")
 
         packetSent = expectedPacketPerNoder / (timeSlots/nodecount)
 
-        print(f"{packetSent=}")
+        # print(f"{packetSent=}")
 
         if packetSent > 1:
             packetSent = 1
 
         maxthroughtimeslots = packetSize / (packetAirtime + gTime)
 
-        print(f"{maxthroughtimeslots=}")
+        # print(f"{maxthroughtimeslots=}")
 
         throughPuttimeslot = maxthroughtimeslots * packetSent
 
-        print(f"{throughPuttimeslot=}")
+        # print(f"{throughPuttimeslot=}")
 
-        throughput = ((throughPuttimeslot * timeSlots) / periodeTime) * 8
+        throughput = throughPuttimeslot * 8
         throughputList.append(throughput)
         print(f"{throughput=}")
 
@@ -60,8 +62,11 @@ def sim():
     y = throughputList
     print(f"{x=}, {y=}")
     plt.plot(x, y)
-    plt.axhline(maxThroughForPeriode, linestyle="dashed",
-                label="Max Throughput", color="red")
+    # plt.axhline(maxThroughputPossible, linestyle="dashed",
+    #             label="Max Throughput Possible", color="red")
+    plt.axhline(maxThroughputForActualSize, linestyle="dashed",
+                label="Max Throughput For Size", color="orange")
+    plt.legend()
     plt.show()
 
 
